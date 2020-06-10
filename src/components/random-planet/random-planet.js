70d97caa74daf90eca7858be0de262f3b5a18742
Margo <margo.bozhenova@gmail.com>
2020-06-11 00:19:51 +0300

    Add random planet rendering

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/spinner';
+import ErrorIndicator from '../error-indicator/';
+import './random-planet.css';
+
+export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+    loading: true,
+    error: false
+  };
+
+  componentDidMount() {
+    this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  onPlanetLoaded = planet => {
+    this.setState({ planet, loading: false });
+  };
+
+  onError = err => {
+    this.setState({ error: true, loading: false });
+  };
+
+  updatePlanet = () => {
+    //the first picture has id = 2, the last one – 19
+    const id = Math.floor(Math.random() * 18) + 2;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
+  };
+
+  render() {
+    const { planet, loading, error } = this.state;
+    const hasData = !(error || loading);
+    const errorMessage = error && <ErrorIndicator />;
+    const spinner = loading && <Spinner />;
+    const content = hasData && <PlanetView planet={planet} />;
+
+    return (
+      <div className='random-planet jumbotron rounded'>
+        {errorMessage}
+        {spinner}
+        {content}
+      </div>
+    );
+  }
+}
+
+const PlanetView = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+
+  return (
+    <>
+      <img
+        className='planet-image'
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+        alt=''
+      />
+      <div>
+        <h4>{name}</h4>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <span className='term'>Population:</span>
+            <span>{population}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Rotation Period:</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Diameter:</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
