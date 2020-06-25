b6a1a09fc9117055f47f69898d1e3772c1c50fee
Margo <margo.bozhenova@gmail.com>
2020-06-26 00:20:35 +0300

    Replace BrowserRouter with HashRouter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,7 +11,7 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { StarshipDetails } from '../sw-components';
 
 export default class App extends Component {
