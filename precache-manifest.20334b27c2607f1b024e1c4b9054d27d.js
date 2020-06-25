20de5c7a5a010f677eb77fa4c2eb4c7fb80ba86a
Margo <margo.bozhenova@gmail.com>
2020-06-26 00:16:42 +0300

    Updates

---
+++ precache-manifest.20334b27c2607f1b024e1c4b9054d27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aec2297defb5891f01f328123af5d3",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "759c11eeb4806a230c53",
+    "url": "/star-db/static/css/main.d78a94eb.chunk.css"
+  },
+  {
+    "revision": "915ca24eb045b46aecc6",
+    "url": "/star-db/static/js/2.beaf0ba6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-db/static/js/2.beaf0ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759c11eeb4806a230c53",
+    "url": "/star-db/static/js/main.e9d38fff.chunk.js"
+  },
+  {
+    "revision": "5e9fe22e0f74b24b7463",
+    "url": "/star-db/static/js/runtime-main.d9f21290.js"
+  }
+]);
